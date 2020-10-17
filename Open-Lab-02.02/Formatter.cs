504b520562b17792afca73bb21736ab991e36c6c
Martin Staňo <martinmatostano@gmail.com>
2020-10-17 16:31:57 +0200

    Add implementation of Format(double[] numbers) of prints to console numbers formatted to 2 decimal places separated with space.

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            Console.Write(string.Format("{0:0.00} {1:0.00} {2:0.00} {3:0.00} {4:0.00}", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]));
         }
     }
 }
